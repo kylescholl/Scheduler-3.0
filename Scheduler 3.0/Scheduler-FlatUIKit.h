d2cdfa33258547484cd8d67b3994e7a089d6a3f7
mccoyLBI <kyle.b.scholl@gmail.com>
2015-11-02 18:08:18 -0500

    Created the project 	- Created a bridging.h file 		- Currently, it doesn't link to anything; this is just a working version that I'll be able to use if "shit hits the fan," so to speak 		- I say that because this is my third new project. All of the other ones have failed miserably and drowned in errors.

---
+++ Scheduler 3.0/Scheduler-FlatUIKit.h
@@ -0,0 +1,14 @@
+//
+//  Scheduler-FlatUIKit.h
+//  Scheduler 3.0
+//
+//  Created by Kyle Scholl on 11/2/15.
+//  Copyright © 2015 Patronus LLC. All rights reserved.
+//
+
+#ifndef Scheduler_FlatUIKit_h
+#define Scheduler_FlatUIKit_h
+
+//#import ""
+
+#endif /* Scheduler_FlatUIKit_h */
