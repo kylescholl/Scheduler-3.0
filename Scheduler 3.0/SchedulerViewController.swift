fe68dc607ee557023f116492eae9aaa92d01b877
mccoyLBI <kyle.b.scholl@gmail.com>
2015-11-03 09:36:11 -0500

    Going to attempt at swapping out the UILabels with UITextFields 	- this will allow individual editing of each block

--- Scheduler 3.0/SchedulerViewController.swift
+++ Scheduler 3.0/SchedulerViewController.swift
@@ -12,6 +12,102 @@ class SchedulerViewController: UIViewController {
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	                  ////////////////
+    	            ////////////////////
+                  ////////////////////////
+                ////////////////////////////
+			  ////////////////////////////////
+            ////////////////////////////////////
+          ////////////////////////////////////////
+        ////////////////////////////////////////////
+      ////////////////////////////////////////////////
+    ////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////
+////////////////// OUTLET COLLECTIONS //////////////////////
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+	
+
+////////////////////////////////////////////////////////////
+////////////// LETTER BLOCKS (diagonal) ////////////////////
+	// Robotics
+	@IBOutlet var aBlockLabelCollection: [UILabel]!
+	
+	// Coding
+	@IBOutlet var bBlockLabelCollection: [UILabel]!
+	
+	// Free / college counseling
+	@IBOutlet var cBlockLabelCollection: [UILabel]!
+	
+	// English
+	@IBOutlet var dBlockLabelCollection: [UILabel]!
+	
+	// Science
+	@IBOutlet var eBlockLabelCollection: [UILabel]!
+	
+	// Math
+	@IBOutlet var fBlockLabelCollection: [UILabel]!
+	
+	// History
+	@IBOutlet var gBlockLabelCollection: [UILabel]!
+	
+	
+	// DAY COLLECTIONS (vertical) //
+	// Day 1
+	@IBOutlet var dayOneGroup: [UILabel]!
+	
+	// Day 2
+	@IBOutlet var dayTwoGroup: [UILabel]!
+	
+	// Day 3
+	@IBOutlet var dayThreeGroup: [UILabel]!
+	
+	// Day 4
+	@IBOutlet var dayFourGroup: [UILabel]!
+	
+	// Day 5
+	@IBOutlet var dayFiveGroup: [UILabel]!
+	
+	// Day 6
+	@IBOutlet var daySixGroup: [UILabel]!
+	
+	// Day 7
+	@IBOutlet var daySevenGroup: [UILabel]!
+	
+	
+	// BUTTON OUTLETS
+	
+	@IBOutlet var dayOneButton: UIButton!
+	
+	@IBOutlet var dayTwoButton: UIButton!
+	
+	@IBOutlet var dayThreeButton: UIButton!
+	
+	@IBOutlet var dayFourButton: UIButton!
+	
+	@IBOutlet var dayFiveButton: UIButton!
+	
+	@IBOutlet var daySixButton: UIButton!
+	
+	@IBOutlet var daySevenButton: UIButton!
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@IBAction func save(sender: UIBarButtonItem) {
 		self.dismissViewControllerAnimated(true, completion: nil)
 	}
