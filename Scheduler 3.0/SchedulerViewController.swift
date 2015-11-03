d0ee8fbbf11bcf72b2b5ab11c5101b3b8654c123
mccoyLBI <kyle.b.scholl@gmail.com>
2015-11-02 21:42:46 -0500

    stuff

---
+++ Scheduler 3.0/SchedulerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SchedulerViewController.swift
+//  Scheduler 3.0
+//
+//  Created by Kyle Scholl on 11/2/15.
+//  Copyright © 2015 Patronus LLC. All rights reserved.
+//
+
+import UIKit
+
+class SchedulerViewController: UIViewController {
+	
+	
+	
+	@IBAction func save(sender: UIBarButtonItem) {
+		self.dismissViewControllerAnimated(true, completion: nil)
+	}
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- Scheduler 3.0/ViewController.swift
+++ Scheduler 3.0/ViewController.swift
@@ -17,33 +17,63 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		print("viewDidLoad()")
-		
-		
 	}
 	
-	func initializeButtonText () {
+	func initializeButtons () {
 		
 		// Schedule
 		let ssb = showScheduleButton
-		ssb.layer.cornerRadius = 20
-		ssb.backgroundColor = UIColor.peterRiverColor()
+		
+		//	ssb.layer.cornerRadius = 10
+		ssb.backgroundColor = UIColor.pumpkinColor()
+		
 		ssb.setTitle("Schedule", forState: UIControlState.Normal)
 		ssb.setTitleColor(UIColor.cloudsColor(), forState: UIControlState.Normal)
-		ssb.titleLabel!.font = UIFont(name: "Helvetica Neue Light", size: 20.0)
+		ssb.titleLabel!.font = UIFont(name: "Helvetica Neue Light", size: 50.0)
 		
 		// NetClassroom
 		let ncb = netClassroomButton
-		ncb.layer.cornerRadius = 20
+		
+		//	ncb.layer.cornerRadius = 10
 		ncb.backgroundColor = UIColor.peterRiverColor()
+		
 		ncb.setTitle("NetClassroom", forState: UIControlState.Normal)
 		ncb.setTitleColor(UIColor.cloudsColor(), forState: UIControlState.Normal)
-		ncb.titleLabel!.font = UIFont(name: "Helvetica Neue Light", size: 20.0)
+		ncb.titleLabel!.font = UIFont(name: "Helvetica Neue Light", size: 50.0)
+		
+		
+		let custom = CGRectMake(0, 362, 375, 5)
+		
+		let whiteLine = UIView(frame: custom)
+		whiteLine.backgroundColor = UIColor.cloudsColor()
+		self.view.addSubview(whiteLine)
 	}
 	
 	
 	
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		
+		// SchedulerViewController
+		if segue.identifier == "SchedulerViewController" {
+			let navigationController = segue.destinationViewController as? UINavigationController
+			let schedulerViewController = navigationController?.topViewController as? SchedulerViewController
+			
+			if let viewController = schedulerViewController {
+				//viewController.delegate = self
+			}
+		}
+		
+		// NetClassroomViewController
+		if segue.identifier == "NetClassroomViewController" {
+			//let navigationController = segue.destinationViewController as? UINavigationController
+			//let netClassroomViewController = navigationController?.topViewController as? NetClassroomViewController
+			
+			/*
+			if let viewController = netClassroomViewController {
+			viewController.delegate = self
+			}
+			*/
+		}
 	}
 	
 	override func viewWillAppear(animated: Bool)  {
@@ -51,7 +81,7 @@ class ViewController: UIViewController {
 		print("viewWillAppear()")
 		
 		self.view.backgroundColor = UIColor.wetAsphaltColor()
-		initializeButtonText()
+		initializeButtons()
 	}
 	
 	func showScheduleButtonPressed(sender: UIButton) {
