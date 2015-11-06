2641c811b0c8cfa27db1dc5861b60efe77d6c169
mccoyLBI <kyle.b.scholl@gmail.com>
2015-11-06 13:52:19 -0500

    lol

--- Scheduler 3.0/SchedulerViewController.swift
+++ Scheduler 3.0/SchedulerViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
+protocol SchedulerViewControllerDelegate {
+	func hehe(hehe: SchedulerViewController, lol: String)
+}
+
 class SchedulerViewController: UIViewController, EditScheduleViewControllerDelegate {
+	
+	var delegate: SchedulerViewControllerDelegate?
 	
 	
 	func aBlockController(aBlockController: EditScheduleViewController, aBlockItem: String){
@@ -179,23 +185,39 @@ class SchedulerViewController: UIViewController, EditScheduleViewControllerDeleg
 	// Day 1
 	@IBOutlet var dayOneGroup: [UITextView]!
 	
+	@IBOutlet var dayOneLabelGroup: [UILabel]!
+	
 	// Day 2
 	@IBOutlet var dayTwoGroup: [UITextView]!
+	
+	@IBOutlet var dayTwoLabelGroup: [UILabel]!
 	
 	// Day 3
 	@IBOutlet var dayThreeGroup: [UITextView]!
 	
+	@IBOutlet var dayThreeLabelGroup: [UILabel]!
+	
 	// Day 4
 	@IBOutlet var dayFourGroup: [UITextView]!
+	
+	@IBOutlet var dayFourLabelGroup: [UILabel]!
 	
 	// Day 5
 	@IBOutlet var dayFiveGroup: [UITextView]!
 	
+	@IBOutlet var dayFiveLabelGroup: [UILabel]!
+	
 	// Day 6
 	@IBOutlet var daySixGroup: [UITextView]!
 	
+	@IBOutlet var daySixLabelGroup: [UILabel]!
+	
 	// Day 7
 	@IBOutlet var daySevenGroup: [UITextView]!
+	
+	@IBOutlet var daySevenLabelGroup: [UILabel]!
+	
+	//	Just realized that I could've just made an even and od groups instead.....oh well
 	
 	
 	// BUTTON OUTLETS
@@ -214,8 +236,9 @@ class SchedulerViewController: UIViewController, EditScheduleViewControllerDeleg
 	
 	@IBOutlet var daySevenButton: UIButton!
 	
+	// ALL LABELS
 	
-	
+	@IBOutlet var allLabelsCollection: [UILabel]!
 	
 	
 	
@@ -287,7 +310,8 @@ class SchedulerViewController: UIViewController, EditScheduleViewControllerDeleg
 		
 		//Create and add an option action
 		let nextAction: UIAlertAction = UIAlertAction(title: "Yes, clear", style: UIAlertActionStyle.Default) { action -> Void in
-			self.setTextViewText("")
+			//self.setTextViewText("")
+			self.setTextToDefault()
 			
 		}
 		actionSheetController.addAction(nextAction)
@@ -297,30 +321,90 @@ class SchedulerViewController: UIViewController, EditScheduleViewControllerDeleg
 	}
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	func setEditingForViews(boolForEditing : Bool) {
+		for index in 0..<6 {
+			aBlockTextViewCollection[index].editable = boolForEditing
+			bBlockTextViewCollection[index].editable = boolForEditing
+			cBlockTextViewCollection[index].editable = boolForEditing
+			dBlockTextViewCollection[index].editable = boolForEditing
+			eBlockTextViewCollection[index].editable = boolForEditing
+			fBlockTextViewCollection[index].editable = boolForEditing
+			gBlockTextViewCollection[index].editable = boolForEditing
+		}
+	}
 	
 	// Edits the schedule
 	@IBAction func editSchedule(sender: UIBarButtonItem) {
 		//Create the AlertController
 		let actionSheetController: UIAlertController = UIAlertController(title: "Edit",
-			message: "Would you like to edit the block groups or each individually?",
+			message: "Are you sure that you want to edit the schedule?",
 			preferredStyle: .Alert)
 		
 		//Create and add the Cancel action
 		let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
-			//Do some stuff
+			self.setEditingForViews(false)
 		}
+		
 		actionSheetController.addAction(cancelAction)
 		
+		//let plot = CGRectMake(5, 20, 320, 400)
+		let alert : FUIAlertView = FUIAlertView()
+		alert.title = "Alert"
+		alert.message = "Here's a message"
+		alert.addButtonWithTitle("Understod")
+		alert.show()
+		
+		
+		
 		//Create and add an option action
-		let nextAction: UIAlertAction = UIAlertAction(title: "Yes, clear", style: UIAlertActionStyle.Default) { action -> Void in
-			self.setTextViewText("")
+		let nextAction: UIAlertAction = UIAlertAction(title: "Edit", style: UIAlertActionStyle.Default) { action -> Void in
+			let editMethodSheetController : UIAlertController = UIAlertController(title: "", message: "Would you like to edit the block groups or each individually?", preferredStyle: .Alert)
 			
+			
+			// Individually
+			let individualEditMethod: UIAlertAction = UIAlertAction(title: "Individually", style: .Cancel) {
+				action -> Void in
+				self.setEditingForViews(true)
+			}
+			
+			
+			
+			editMethodSheetController.addAction(individualEditMethod)
+			
+			
+			// Segue to ViewController
+			let blockEditMethod: UIAlertAction = UIAlertAction(title: "Groups", style: UIAlertActionStyle.Default) {
+				action -> Void in
+				self.performSegueWithIdentifier("editScheduleViewController", sender: self)
+			}
+			
+			editMethodSheetController.addAction(blockEditMethod)
+			
+			self.presentViewController(editMethodSheetController, animated: true, completion: nil)
 		}
+		
 		actionSheetController.addAction(nextAction)
+		
 		
 		//Present the AlertController
 		self.presentViewController(actionSheetController, animated: true, completion: nil)
 	}
+	
+	
+	
+	
+	
 	
 	@IBAction func back(sender: UIBarButtonItem) {
 		self.dismissViewControllerAnimated(true, completion: nil)
@@ -328,46 +412,13 @@ class SchedulerViewController: UIViewController, EditScheduleViewControllerDeleg
 	
 	func setTextViewText(someString: String) {
 		
-		let aNumIndex : Int = aBlockTextViewCollection.count
-		let bNumIndex : Int = bBlockTextViewCollection.count
-		let cNumIndex : Int = cBlockTextViewCollection.count
-		let dNumIndex : Int = dBlockTextViewCollection.count
-		let eNumIndex : Int = eBlockTextViewCollection.count
-		let fNumIndex : Int = fBlockTextViewCollection.count
-		let gNumIndex : Int = gBlockTextViewCollection.count
-		
-		// A
-		for index in 0..<aNumIndex {
+		for index in 0..<6 {
 			aBlockTextViewCollection[index].text = someString
-		}
-		
-		// B
-		for index in 0..<bNumIndex {
 			bBlockTextViewCollection[index].text = someString
-		}
-		
-		// C
-		for index in 0..<cNumIndex {
 			cBlockTextViewCollection[index].text = someString
-		}
-		
-		// D
-		for index in 0..<dNumIndex {
 			dBlockTextViewCollection[index].text = someString
-		}
-		
-		// E
-		for index in 0..<eNumIndex {
 			eBlockTextViewCollection[index].text = someString
-		}
-		
-		// F
-		for index in 0..<fNumIndex {
 			fBlockTextViewCollection[index].text = someString
-		}
-		
-		// G
-		for index in 0..<gNumIndex {
 			gBlockTextViewCollection[index].text = someString
 		}
 	}
@@ -388,12 +439,213 @@ class SchedulerViewController: UIViewController, EditScheduleViewControllerDeleg
     override func viewDidLoad() {
         super.viewDidLoad()
 		NSLog("SchedulerViewController: viewDidLoad()")
-
-        // Do any additional setup after loading the view.
+		
+		setEditingForViews(false)
     }
+	
+	override func viewWillAppear(animated: Bool) {
+		setTextToDefault()
+		initializeAllUIElements()
+		
+		self.view.backgroundColor = UIColor.wetAsphaltColor()
+	}
+	
+	func setTextToDefault() {
+		
+		let aNumIndex : Int = aBlockTextViewCollection.count
+		let bNumIndex : Int = bBlockTextViewCollection.count
+		let cNumIndex : Int = cBlockTextViewCollection.count
+		let dNumIndex : Int = dBlockTextViewCollection.count
+		let eNumIndex : Int = eBlockTextViewCollection.count
+		let fNumIndex : Int = fBlockTextViewCollection.count
+		let gNumIndex : Int = gBlockTextViewCollection.count
+		
+		// A
+		for index in 0..<aNumIndex {
+			aBlockTextViewCollection[index].text = "A"
+		}
+		
+		// B
+		for index in 0..<bNumIndex {
+			bBlockTextViewCollection[index].text = "B"
+		}
+		
+		// C
+		for index in 0..<cNumIndex {
+			cBlockTextViewCollection[index].text = "C"
+		}
+		
+		// D
+		for index in 0..<dNumIndex {
+			dBlockTextViewCollection[index].text = "D"
+		}
+		
+		// E
+		for index in 0..<eNumIndex {
+			eBlockTextViewCollection[index].text = "E"
+		}
+		
+		// F
+		for index in 0..<fNumIndex {
+			fBlockTextViewCollection[index].text = "F"
+		}
+		
+		// G
+		for index in 0..<gNumIndex {
+			gBlockTextViewCollection[index].text = "G"
+		}
+	}
+	
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+	
+	
+	
+	
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////// The following is all /////////////////////////////
+/////////////////////////// styling and customization //////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+	
+	
+	@IBOutlet var navigationBar: UINavigationItem!
+	
+	@IBOutlet var backButton: UINavigationItem!
+	
+	@IBOutlet var clearButton: UIBarButtonItem!
+	
+	@IBOutlet var editButton: UIBarButtonItem!
+	
+	
+	
+	
+	func initializeAllUIElements() {
+		
+		let textColor : UIColor = UIColor.cloudsColor()
+		let backColor : UIColor = UIColor.wetAsphaltColor()
+		
+		// Sets the textViews
+	/*
+		for index in 0..<6 {
+			var elem : UITextView
+			
+			elem = dayOneGroup[index]
+			elem.backgroundColor = UIColor.wetAsphaltColor()
+			elem.textColor = textColor
+			
+			elem = dayTwoGroup[index]
+			elem.backgroundColor = UIColor.midnightBlueColor()
+			elem.textColor = textColor
+		
+			elem = dayThreeGroup[index]
+			elem.backgroundColor = UIColor.wetAsphaltColor()
+			elem.textColor = textColor
+			
+			elem = dayFourGroup[index]
+			elem.backgroundColor = UIColor.midnightBlueColor()
+			elem.textColor = textColor
+			
+			elem = dayFiveGroup[index]
+			elem.backgroundColor = UIColor.wetAsphaltColor()
+			elem.textColor = textColor
+			
+			elem = daySixGroup[index]
+			elem.backgroundColor = UIColor.midnightBlueColor()
+			elem.textColor = textColor
+			
+			elem = daySevenGroup[index]
+			elem.backgroundColor = UIColor.wetAsphaltColor()
+			elem.textColor = textColor
+		}
+	*/
+		
+		for index in 0..<6 {
+			var elem : UITextView
+			var curElem : UITextView
+			
+			elem = dayOneGroup[index]
+			//elem.backgroundColor = backColor
+			elem.textColor = textColor
+			
+			elem = dayTwoGroup[index]
+			//elem.backgroundColor = backColor
+			elem.textColor = textColor
+			
+			elem = dayThreeGroup[index]
+			//elem.backgroundColor = backColor
+			elem.textColor = textColor
+			
+			elem = dayFourGroup[index]
+			//elem.backgroundColor = backColor
+			elem.textColor = textColor
+			
+			curElem = dayFiveGroup[index]
+			elem = curElem
+			//elem.backgroundColor = backColor
+			elem.textColor = textColor
+			
+			elem = daySixGroup[index]
+			//elem.backgroundColor = backColor
+			elem.textColor = textColor
+			
+			elem = daySevenGroup[index]
+			////elem.backgroundColor = backColor
+			elem.textColor = textColor
+		}
+		
+
+		for index in 0..<allLabelsCollection.count {
+			//allLabelsCollection[index].backgroundColor = backColor
+			
+			allLabelsCollection[index].textColor = textColor
+			allLabelsCollection[index].font = UIFont(name: "Helvetica Neue Thin", size: 12.0)
+		}
+	}
+	
+	
+	func customizeBarButtons() {
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- Scheduler 3.0/ViewController.swift
+++ Scheduler 3.0/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
+	
+	func hehe(hehe: SchedulerViewController, lol: String) {
+	
+	}
+	
 	
 	@IBOutlet var showScheduleButton: UIButton!
 	@IBOutlet var netClassroomButton: UIButton!
@@ -19,13 +23,14 @@ class ViewController: UIViewController {
 		print("viewDidLoad()")
 	}
 	
-	func initializeButtons () {
+	func setButtonStyles() {
 		
 		// Schedule
 		let ssb = showScheduleButton
 		
 		//	ssb.layer.cornerRadius = 10
-		ssb.backgroundColor = UIColor.pumpkinColor()
+		ssb.backgroundColor = UIColor.orangeColor()
+		//ssb.backgroundColor = UIColor.belizeHoleColor()
 		
 		ssb.setTitle("Schedule", forState: UIControlState.Normal)
 		ssb.setTitleColor(UIColor.cloudsColor(), forState: UIControlState.Normal)
@@ -35,7 +40,8 @@ class ViewController: UIViewController {
 		let ncb = netClassroomButton
 		
 		//	ncb.layer.cornerRadius = 10
-		ncb.backgroundColor = UIColor.peterRiverColor()
+		//ncb.backgroundColor = UIColor.peterRiverColor()
+		ncb.backgroundColor = UIColor.orangeColor()
 		
 		ncb.setTitle("NetClassroom", forState: UIControlState.Normal)
 		ncb.setTitleColor(UIColor.cloudsColor(), forState: UIControlState.Normal)
@@ -46,17 +52,66 @@ class ViewController: UIViewController {
 		
 		let whiteLine = UIView(frame: custom)
 		whiteLine.backgroundColor = UIColor.cloudsColor()
-		self.view.addSubview(whiteLine)
+		//self.view.addSubview(whiteLine)
 	}
+	
+	
+	@IBAction func showScheduleButtonPressed(sender: UIButton) {
+		NSLog("showScheduleButtonPressed")
+		
+		self.performSegueWithIdentifier("SchedulerViewController", sender: self)
+	}
+	
+	
+	
+	
+	@IBAction func animateButtons(sender: UIButton) {
+		
+		let button = sender as UIButton
+		
+		button.addTarget(self, action: "pressed:", forControlEvents: .TouchUpInside)
+		button.addTarget(self, action: "touchEnded", forControlEvents: .TouchCancel)
+		
+
+/*
+		// resets the color back to normal
+		UIView.animateWithDuration(1.0, animations: {
+			button.backgroundColor = UIColor.orangeColor()
+		})
+*/
+	}
+	
+	func pressed(sender: UIButton!) {
+		
+		let button = sender as UIButton
+		
+		// darken the button's color
+		UIView.animateWithDuration(2.0, animations: {
+			button.backgroundColor = UIColor.pumpkinColor()
+		})
+	}
+	
+	func touchEnded(sender: UIButton!) {
+		print("touchEnded() ran")
+	}
+	
+	
+	
+	
+	
 	
 	
 	
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		
 		// SchedulerViewController
-		if segue.identifier == "SchedulerViewController" {
-			//let navigationController = segue.destinationViewController as? UINavigationController
-			//let schedulerViewController = navigationController?.topViewController as? SchedulerViewController
+		if segue.identifier == "AddItemViewController" {
+			let navigationController = segue.destinationViewController as? UINavigationController
+			let schedulerViewController = navigationController?.topViewController as? SchedulerViewController
+			
+			if let viewController = schedulerViewController {
+				//viewController.delegate = self
+			}
 		}
 		
 		// NetClassroomViewController
@@ -77,17 +132,42 @@ class ViewController: UIViewController {
 		print("viewWillAppear()")
 		
 		self.view.backgroundColor = UIColor.wetAsphaltColor()
-		initializeButtons()
+		setButtonStyles()
 	}
 	
-	func showScheduleButtonPressed(sender: UIButton) {
-		NSLog("showScheduleButtonPressed")
-	}
 	
-	func netClassroomButtonPressed(sender: UIButton) {
+	
+	
+	
+	
+	
+	
+	@IBAction func netClassroomButtonPressed(sender: UIButton) {
 		NSLog("netClassroomButtonPressed")
+		
+		
+		
+		//let plot = CGRectMake(5, 20, 320, 400)
+		let alert : FUIAlertView = FUIAlertView()
+		alert.title = "Alert"
+		alert.message = "Here's a message"
+		alert.addButtonWithTitle("Understod")
+		alert.defaultButtonColor = UIColor.wetAsphaltColor()
+		
+		alert.show()
+		
+		
+		
+		
 	}
-
+	
+	
+	
+	
+	
+	
+	
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
